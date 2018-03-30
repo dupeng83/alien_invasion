92d897580ef9e0d55e5645af73bd05e29eeda9a3
Du Peng <dupeng83@gmail.com>
2018-03-30 20:13:24 +0800

    可以向右移动了，但不能连续移动

--- youxi.py
+++ youxi.py
@@ -7,7 +7,11 @@ screen = pygame.display.set_mode((1000, 400))
 screen_rect = screen.get_rect()
 
 feichuan_image = pygame.image.load('jiantou.bmp')
+
 feichuan_rect = feichuan_image.get_rect()
+
+feichuan_rect.centerx = screen_rect.centerx
+feichuan_rect.bottom = screen_rect.bottom
 
 print(feichuan_rect)
 
@@ -15,6 +19,9 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                feichuan_rect.centerx += 3
 
     screen.fill((255, 255, 255))
     screen.blit(feichuan_image, feichuan_rect)
